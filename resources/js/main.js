bbe4ea2b05fbef207875ac339e91c345d16d7110
Jorg van der Schaaf <JVDS@MacBook-Air-van-Jorg-3958.local>
2017-06-29 00:03:18 +0200

    added js file for carousel and animate navbar

---
+++ resources/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+
+  var x = 0;
+  $(window).scroll(function(){
+    var y = $(this).scrollTop();
+    if (y - x > 50){
+      var z = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + z}, 150);
+      x = y;
+    } else if (x - y > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      x = y;
+    }
+  });
+
+});
