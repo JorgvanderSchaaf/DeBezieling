b41a7ffefda0bd5d81bbcce48d7a43b22334bad3
Jorg van der Schaaf <JVDS@MacBook-Air-van-Jorg-3958.local>
2017-07-04 00:21:48 +0200

    changed carousel speed to 6 seconds

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   $('.carousel').slick({
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 4000,
+    autoplaySpeed: 6000,
     arrows: false,
     draggable: false,
     pauseOnFocus: false,
